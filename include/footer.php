6a22f11ebbc410267d54f503d8c1b844e630f058
HiromuK <saga.cap319@gmail.com>
2016-08-30 15:46:12 +0900

    内部リンク修正

--- include/footer.php
+++ include/footer.php
@@ -1,20 +1,20 @@
 <!-- navigation -->
 <nav style="margin-top: 30px; margin-bottom: 30px;"><span id="global" class="footer_subnav global2"><span class="menu-waku">MENU</span></span>
 	<ul id="nav" class="footer_subnav nav2">
-        <li><a href="http://a-dreamlaw.com/flow/">ご相談の流れ</a></li>
-        <li><a href="http://a-dreamlaw.com/business/divorce/">離婚・相続問題</a>
+        <li><a href="/flow/">ご相談の流れ</a></li>
+        <li><a href="/business/divorce/">離婚・相続問題</a>
         <span id="s1"></span></li>
-        <li><a href="http://a-dreamlaw.com/business/building/">不動産・建築問題</a>
+        <li><a href="/business/building/">不動産・建築問題</a>
         <span id="s2"></span></li>
-        <li><a href="http://a-dreamlaw.com/lawyer/">弁護士紹介</a>
+        <li><a href="/lawyer/">弁護士紹介</a>
         <span id="s3"></span></li>
-        <li><a href="http://a-dreamlaw.com/business/case-index.html">解決事例</a>
+        <li><a href="/business/case-index.html">解決事例</a>
         <span id="s4"></span></li>
-        <li><a href="http://a-dreamlaw.com/fee/">弁護士費用</a>
+        <li><a href="/fee/">弁護士費用</a>
         <span id="s5"></span></li>
-        <li><a href="http://a-dreamlaw.com/office">事務所紹介</a>
+        <li><a href="/office">事務所紹介</a>
         <span id="s6"></span></li>
-        <li class="pc_noactive"><a href="http://a-dreamlaw.com/office/index.html#maplink">アクセス</a>
+        <li class="pc_noactive"><a href="/office/index.html#maplink">アクセス</a>
         </li>
     </ul>
 </nav>
@@ -27,7 +27,7 @@
 			</div>
 		</div>
 		<div class="gutters col span_4">
-			<div class="btn"><a class="contact_btn" href="http://a-dreamlaw.com/contact/" id="form_pctop_area_gtm">
+			<div class="btn"><a class="contact_btn" href="/contact/" id="form_pctop_area_gtm">
 				<span>メールでのお問い合わせ</span></a>
 			</div>
 		</div>
@@ -42,9 +42,9 @@
 <div class="contact_banner_sp">
 	<div class="contact_banner-tel">
 		<p style="padding: 5px;">無料相談実施中</p>
-		<a onclick="goog_report_conversion('tel:0335258010');ga('send', 'event', 'adreamlaw', 'tel', 'label');return false;" href="tel:0335258010" ><img src="http://a-dreamlaw.com/common/images/header_tel.jpg" alt="電話でのお問い合わせ"></a>
+		<a onclick="goog_report_conversion('tel:0335258010');ga('send', 'event', 'adreamlaw', 'tel', 'label');return false;" href="tel:0335258010" ><img src="/common/images/header_tel.jpg" alt="電話でのお問い合わせ"></a>
 	</div>
-	<div class="btn"><a class="contact_btn" href="http://a-dreamlaw.com/contact/" id="form_sptop_area_gtm">
+	<div class="btn"><a class="contact_btn" href="/contact/" id="form_sptop_area_gtm">
 				<span>メールでのお問い合わせ</span></a>
 	</div>
 </div>
@@ -52,56 +52,56 @@
 		<div class="footer_inner container row">
 			<div class="footer_box">
 				<ul class="footer_arrow">
-					<li><a href="http://a-dreamlaw.com/office/">当法人について</a></li>
-					<li><a href="http://a-dreamlaw.com/lawyer/">弁護士紹介</a></li>
-					<li><a href="http://a-dreamlaw.com/flow/">ご相談の流れ</a></li>
-					<li><a href="http://a-dreamlaw.com/fee/">弁護士費用</a></li>
-					<li><a href="http://a-dreamlaw.com/contact/" id="form_topfoot_gtm">お問い合わせ</a></li>
-					<li><a href="http://a-dreamlaw.com/sitemap/">サイトマップ</a></li>
-					<li><a href="http://a-dreamlaw.com/public/">プライバシーポリシー</a></li>
+					<li><a href="/office/">当法人について</a></li>
+					<li><a href="/lawyer/">弁護士紹介</a></li>
+					<li><a href="/flow/">ご相談の流れ</a></li>
+					<li><a href="/fee/">弁護士費用</a></li>
+					<li><a href="/contact/" id="form_topfoot_gtm">お問い合わせ</a></li>
+					<li><a href="/sitemap/">サイトマップ</a></li>
+					<li><a href="/public/">プライバシーポリシー</a></li>
 				</ul>
 			</div>
 			<div class="footer_box">
-				<p class="footer_h"><a href="http://a-dreamlaw.com/business/divorce/">離婚・相続問題（家事）</a></p>
+				<p class="footer_h"><a href="/business/divorce/">離婚・相続問題（家事）</a></p>
 				<ul class="footer_arrow">
-					<li><a href="http://a-dreamlaw.com/business/divorce/men/">男性のための離婚</a></li>
-					<li><a href="http://a-dreamlaw.com/business/divorce/women/">女性のための離婚</a></li>
-					<li><a href="http://a-dreamlaw.com/business/divorce/case/">解決事例</a></li>
-					<li><a href="http://a-dreamlaw.com/business/divorce/type/">離婚の種類</a></li>
-					<li><a href="http://a-dreamlaw.com/business/divorce/money/">離婚とお金</a></li>
-					<li><a href="http://a-dreamlaw.com/business/divorce/qa/">離婚問題のQ&amp;A</a></li>
+					<li><a href="/business/divorce/men/">男性のための離婚</a></li>
+					<li><a href="/business/divorce/women/">女性のための離婚</a></li>
+					<li><a href="/business/divorce/case/">解決事例</a></li>
+					<li><a href="/business/divorce/type/">離婚の種類</a></li>
+					<li><a href="/business/divorce/money/">離婚とお金</a></li>
+					<li><a href="/business/divorce/qa/">離婚問題のQ&amp;A</a></li>
 				</ul>
 			</div>
 			<div class="footer_box">
-				<p class="footer_h"><a href="http://a-dreamlaw.com/business/building/">不動産・建築（企業法務）</a></p>
+				<p class="footer_h"><a href="/business/building/">不動産・建築（企業法務）</a></p>
 				<ul class="footer_arrow">
-					<li><a href="http://a-dreamlaw.com/business/building/case/">解決事例</a></li>
-					<li><a href="http://a-dreamlaw.com/business/building/seminar/">セミナー案内</a></li>
-					<li><a href="http://a-dreamlaw.com/business/building/adviser/">顧問のご案内</a></li>
+					<li><a href="/business/building/case/">解決事例</a></li>
+					<li><a href="/business/building/seminar/">セミナー案内</a></li>
+					<li><a href="/business/building/adviser/">顧問のご案内</a></li>
 				</ul>
 				<p class="footer_h">その他</p>
 				<ul class="footer_arrow">
-					<li><a href="http://a-dreamlaw.com/info/">メディア情報</a></li>
+					<li><a href="/info/">メディア情報</a></li>
 				</ul>
 			</div>
 			<div class="footer_link-sns-box">
-					<p><a href="http://ameblo.jp/awajichodream/" rel="nofollow" target="blank" class="fade"><img src="http://a-dreamlaw.com/common/images/blog.jpg" alt="淡路町ドリーム公式ブログ"></a></p>
-					<p><a href="http://myben54.com/" rel="nofollow" target="blank" class="fade"><img src="http://a-dreamlaw.com/common/images/myben54bannar.jpg" alt="MY弁護士"></a></p>
+					<p><a href="http://ameblo.jp/awajichodream/" rel="nofollow" target="blank" class="fade"><img src="/common/images/blog.jpg" alt="淡路町ドリーム公式ブログ"></a></p>
+					<p><a href="http://myben54.com/" rel="nofollow" target="blank" class="fade"><img src="/common/images/myben54bannar.jpg" alt="MY弁護士"></a></p>
 				<ul>
-					<li><a href="https://ja-jp.facebook.com/pages/%E5%BC%81%E8%AD%B7%E5%A3%AB%E6%B3%95%E4%BA%BA%E6%B7%A1%E8%B7%AF%E7%94%BA%E3%83%89%E3%83%AA%E3%83%BC%E3%83%A0/1440368016199684" rel="nofollow" class="fade" target="blank"><img src="http://a-dreamlaw.com/common/images/icon/facebook.jpg" width="50" height="50" alt="淡路町ドリームfacebook公式アカウント"></a></li>
-					<li><a href="http://twitter.com/adreamlaw" rel="nofollow" class="fade" target="blank"><img src="http://a-dreamlaw.com/common/images/icon/twitter.jpg" width="50" height="50" alt="淡路町ドリームTwitter公式アカウント"></a></li>
+					<li><a href="https://ja-jp.facebook.com/pages/%E5%BC%81%E8%AD%B7%E5%A3%AB%E6%B3%95%E4%BA%BA%E6%B7%A1%E8%B7%AF%E7%94%BA%E3%83%89%E3%83%AA%E3%83%BC%E3%83%A0/1440368016199684" rel="nofollow" class="fade" target="blank"><img src="/common/images/icon/facebook.jpg" width="50" height="50" alt="淡路町ドリームfacebook公式アカウント"></a></li>
+					<li><a href="http://twitter.com/adreamlaw" rel="nofollow" class="fade" target="blank"><img src="/common/images/icon/twitter.jpg" width="50" height="50" alt="淡路町ドリームTwitter公式アカウント"></a></li>
 				</ul>
 			</div>
 		</div>
 </footer>
 <footer class="sp_footer row">
 		<ul>
-			<li><a href="http://a-dreamlaw.com/sitemap/">サイトマップ</a></li>
-			<li><a href="http://a-dreamlaw.com/public/">プライバシーポリシー</a></li>
+			<li><a href="/sitemap/">サイトマップ</a></li>
+			<li><a href="/public/">プライバシーポリシー</a></li>
 		</ul>
 			<ul class="footer_link-sns-box_sp row">
-				<li><a href="https://ja-jp.facebook.com/pages/%E5%BC%81%E8%AD%B7%E5%A3%AB%E6%B3%95%E4%BA%BA%E6%B7%A1%E8%B7%AF%E7%94%BA%E3%83%89%E3%83%AA%E3%83%BC%E3%83%A0/1440368016199684" rel="nofollow" target="blank"><img src="http://a-dreamlaw.com/common/images/icon/facebook.jpg" width="50" height="50" alt="淡路町ドリームfacebook公式アカウント"></a></li>
-				<li><a href="http://twitter.com/adreamlaw" rel="nofollow" target="blank"><img src="http://a-dreamlaw.com/common/images/icon/twitter.jpg" width="50" height="50" alt="淡路町ドリームTwitter公式アカウント"></a></li>
+				<li><a href="https://ja-jp.facebook.com/pages/%E5%BC%81%E8%AD%B7%E5%A3%AB%E6%B3%95%E4%BA%BA%E6%B7%A1%E8%B7%AF%E7%94%BA%E3%83%89%E3%83%AA%E3%83%BC%E3%83%A0/1440368016199684" rel="nofollow" target="blank"><img src="/common/images/icon/facebook.jpg" width="50" height="50" alt="淡路町ドリームfacebook公式アカウント"></a></li>
+				<li><a href="http://twitter.com/adreamlaw" rel="nofollow" target="blank"><img src="/common/images/icon/twitter.jpg" width="50" height="50" alt="淡路町ドリームTwitter公式アカウント"></a></li>
 			</ul>
 			<p class="txt_C">&copy; Copyright 2016 弁護士法人 淡路町ドリーム All Rights Reserved.</p>
 </footer>
