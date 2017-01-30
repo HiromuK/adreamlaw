5300677f7f27c2e69b58e61a4e85685458c36f94
Masterwood <kimura.unit@gmail.com>
2017-01-30 19:38:55 +0900

    グロナビのインクルード化

--- divorce/include/sidecol-left.php
+++ divorce/include/sidecol-left.php
@@ -1,36 +1,27 @@
 <div class="column__left">
   <section>
 
-    <section class="sidecolumn__border">
+    <section class="centraize sidecolumn__button-fade">
       <a href="/contact/">
-        <h3 class="centraize sidecolumn__subhedding sidecolumn__emphasis sidecolumn__subhedding-orange"><strong>はじめての離婚相談<br><b>60分無料</b></strong></h3>
-        <p class="centraize sidecontents__paragraph-only">平日21時まで</p>
-        <p class="centraize sidecontents__paragraph-only">土日祝日も対応</p>
-        <p class="centraize sidecontents__paragraph-only">男女別選択可</p>
-        <p class="centraize sidecontents__paragraph-only">駅徒歩1分</p>
-        <div class="centraize">
-          <img src="https://a-dreamlaw.com/common/images/logo.png" class="sidecontents__logo">
-        </div>
-        <p class="centraize sidecontents__paragraph-only"><b>03-3525-8010</b></p>
-        <p class="centraize sidecolumn__paragraph-orange"><strong>メールはこちら</strong></p>
+        <img src="/divorce/images/side_CTA.jpg">
       </a>
     </section>
 
     <section>
       <a href="/divorce/contents/merit.html">
-      <div class="sidecolumn__button-fade sidecolumn__border">
-        <h3 class="centraize sidecolumn__subhedding sidecontents__paragraph">「離婚」が頭をよぎったら</h3>
-        <p class="centraize sidecolumn__emphasis sidecontents__paragraph-only">弁護士に早めに相談するメリット</p>
-      </div>
+        <div class="sidecolumn__button-fade sidecolumn__border">
+          <h3 class="centraize sidecolumn__subhedding sidecontents__paragraph">「離婚」が頭をよぎったら</h3>
+          <p class="centraize sidecolumn__emphasis sidecontents__paragraph-only">弁護士に早めに相談するメリット</p>
+        </div>
       </a>
     </section>
 
     <section>
       <a href="/divorce/contents/first-consultation.html">
-      <div class="sidecolumn__border sidecolumn__button-fade">
-        <h3 class="centraize sidecolumn__subhedding sidecontents__paragraph">はじめての離婚相談</h3>
-        <p class="centraize sidecolumn__emphasis sidecontents__paragraph-only">ご相談前の疑問・不安にお答えします。</p>
-      </div>
+        <div class="sidecolumn__border sidecolumn__button-fade">
+          <h3 class="centraize sidecolumn__subhedding sidecontents__paragraph">はじめての離婚相談</h3>
+          <p class="centraize sidecolumn__emphasis sidecontents__paragraph-only">ご相談前の疑問・不安にお答えします。</p>
+        </div>
       </a>
     </section>
 
@@ -41,130 +32,130 @@
 
     <section>
       <a href="/divorce/contents/actual-compensation.html">
-      <div class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">請求したい？請求された？</h4>
-        <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">慰謝料のホント</p>
-      </div>
+        <div class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">請求したい？請求された？</h4>
+          <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">慰謝料のホント</p>
+        </div>
       </a>
     </section>
 
     <section>
-      <a href="/divorce/contents/divorce-money.html">
-      <div class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">考えるべきこと</h4>
-        <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">離婚とお金</p>
-      </div>
+      <a href="/divorce/contents/">
+        <div class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">考えるべきこと</h4>
+          <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">離婚とお金</p>
+        </div>
       </a>
     </section>
 
     <section>
-      <a href="/divorce/contents/divorce-child.html">
-      <div class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">考えるべきこと</h4>
-        <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">離婚と子ども</p>
-      </div>
+      <a href="/divorce/contents/">
+        <div class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">考えるべきこと</h4>
+          <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">離婚と子ども</p>
+        </div>
       </a>
     </section>
 
     <section>
       <a href="/divorce/contents/separate.html">
-      <div  class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">「別居」が意味するもの</h4>
-      </div>
+        <div  class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">「別居」が意味するもの</h4>
+        </div>
       </a>
     </section>
 
     <section>
       <a href="/divorce/contents/qa/">
-      <div class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">離婚Q＆A</h4>
-      </div>
+        <div class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">離婚Q＆A</h4>
+        </div>
       </a>
     </section>
 
     <section>
-      <a href="/divorce/case/">
-      <div class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">解決事例</h4>
-      </div>
+      <a href="/business/divorce/case/">
+        <div class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">解決事例</h4>
+        </div>
       </a>
     </section>
 
     <section class="sidecontents__border">
       <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">離婚の知識と法律</h4>
       <ul class="sidecontents__lists">
-        <li><a href="/divorce/contents/factor.html">離婚原因</a></li>
-        <li><a href="/divorce/contents/type/">離婚の種類</a></li>
-        <ul class="sidecontents__lists-child">
-          <li><a href="/divorce/contents/type/consent.html">協議離婚</a></li>
-          <li><a href="/divorce/contents/type/arbitration.html">調停離婚</a></li>
-          <li><a href="/divorce/contents/type/judgement.html">審判離婚</a></li>
-          <li><a href="/divorce/contents/type/adjudicated.html">裁判離婚</a></li>
-        </ul>
-        <li><a href="/divorce/contents/flow.html">離婚手続きの流れ</a></li>
-        <li><a href="/divorce/contents/kyougisyo.html">離婚協議書</a></li>
+        <li>離婚原因</li>
+        <li><a href="/business/divorce/type/">離婚の種類</a></li>
+          <ul class="sidecontents__lists-child">
+            <li><a href="/business/divorce/type/consent.html">協議離婚</a></li>
+            <li><a href="/business/divorce/type/arbitration.html">調停離婚</a></li>
+            <li><a href="/business/divorce/type/judgement.html">審判離婚</a></li>
+            <li><a href="/business/divorce/type/adjudicated.html">裁判離婚</a></li>
+          </ul>
+        <li>離婚手続きの流れ</li>
+        <li><a href="/fee/why/kyougisyo.html">離婚協議書</a></li>
         <li><a href="/divorce/contents/money/">離婚とお金</a></li>
-        <ul class="sidecontents__lists-child">
-          <li><a href="/divorce/contents/actual-compensation.html">慰謝料</a></li>
-          <li><a href="/divorce/contents/money/distribution.html">財産分与</a></li>
-          <li><a href="/divorce/contents/money/notification.html">婚姻費用</a></li>
-          <li><a href="/divorce/contents/money/child_support.html">養育費</a></li>
-          <li><a href="/divorce/contents/money/loan.html">住宅ローン</a></li>
-          <li><a href="/divorce/contents/money/pension.html">年金分割</a></li>
-          <li><a href="/divorce/contents/money/enforcement.html">強制執行</a></li>
-        </ul>
+          <ul class="sidecontents__lists-child">
+            <li><a href="/divorce/contents/actual-compensation.html">慰謝料</a></li>
+            <li><a href="/divorce/contents/money/distribution.html">財産分与</a></li>
+            <li><a href="/divorce/contents/money/notification.html">婚姻費用</a></li>
+            <li><a href="/divorce/contents/money/child_support.html">養育費</a></li>
+            <li><a href="/divorce/contents/money/loan.html">住宅ローン</a></li>
+            <li><a href="/divorce/contents/money/pension.html">年金分割</a></li>
+            <li><a href="/divorce/contents/money/enforcement.html">強制執行</a></li>
+          </ul>
         <li><a href="/divorce/contents/child/">離婚と子供</a></li>
-        <ul class="sidecontents__lists-child">
-          <li><a href="/divorce/contents/child/custody01.html">親権</a></li>
-          <li><a href="/divorce/contents/child/custody02.html">監護権</a></li>
-          <li><a href="/divorce/contents/child/visitation.html">面会交流権</a></li>
-          <li><a href="/divorce/contents/child/habeas.html">子の引渡し請求</a></li>
-        </ul>
-        <li><a href="divorce/contents/domestic-violence.html">DV (ドメスティックバイオレンス)</a></li>
+          <ul class="sidecontents__lists-child">
+            <li><a href="/divorce/contents/child/custody01.html">親権</a></li>
+            <li><a href="/divorce/contents/child/custody02.html">監護権</a></li>
+            <li><a href="/divorce/contents/child/visitation.html">面会交流権</a></li>
+            <li><a href="/divorce/contents/child/habeas.html">子の引渡し請求</a></li>
+          </ul>
+        <li><a href="divorce/contents/dv.html">DV (ドメスティックバイオレンス)</a></li>
         <li><a href="/divorce/contents/morahara.html">モラルハラスメント</a></li>
       </ul>
     </section>
 
     <section>
       <a href="/fee/">
-      <div class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">裁判や調停の「日当」なしで安心</h4>
-        <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">弁護士費用</p>
-      </div>
+        <div class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">裁判や調停の「日当」なしで安心</h4>
+          <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">弁護士費用</p>
+        </div>
       </a>
     </section>
 
     <section>
       <a href="/info/">
-      <div class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">メディア実績</h4>
-      </div>
+        <div class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">メディア実績</h4>
+        </div>
       </a>
     </section>
 
     <section>
       <a href="/business/divorce/immorality/">
-      <div class="sidecontents__border sidecolumn__button-fade">
-        <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">離婚弁護士の選び方<br>（動画）</h4>
-      </div>
+        <div class="sidecontents__border sidecolumn__button-fade">
+          <h4 class="centraize sidecontents__subhedding sidecontents__emphasis sidecontents__paragraph-only">離婚弁護士の選び方<br>（動画）</h4>
+        </div>
       </a>
     </section>
-  </section>
-</div>
-<div class="lp4__sumaho-footer">
-  <section class="lp4__sumaho-display">
-    <div class="contact__wrapper">
-      <div class="contact__wrapper-border">
-        <div class="contact__sub-wrapper-left">
-          <h3 class="centraize">1回の相談で見えてくることがあります。</h3>
-          <p class="centraize contact__sales-bold">「はじめての離婚相談」60分無料</p>
-          <p class="contact__sales-note centraize">ご相談前の疑問・不安にお答えします。</p>
-        </div>
-        <div class="contact__sub-wrapper-right">
-          <a href="tel:0335258010"><div class="contact__button"><p class="centraize">法律相談のご予約は<br>03-3525-8010</p></div></a>
-          <a href="/contact/" class="lp4__link"><div class="contact__button"><p class="centraize">メールでのお問い合わせ</p></div></a>
+  </div>
+  <div class="lp4__sumaho-footer">
+    <section class="lp4__sumaho-display">
+      <div class="contact__wrapper">
+        <div class="contact__wrapper-border">
+          <div class="contact__sub-wrapper-left">
+            <h3 class="centraize">1回の相談で見えてくることがあります。</h3>
+            <p class="centraize contact__sales-bold">「はじめての離婚相談」60分無料</p>
+            <p class="contact__sales-note centraize">ご相談前の疑問・不安にお答えします。</p>
+          </div>
+          <div class="contact__sub-wrapper-right">
+            <a href="tel:0335258010"><div class="contact__button"><p class="centraize">法律相談のご予約は<br>03-3525-8010</p></div></a>
+            <a href="/contact/" class="lp4__link"><div class="contact__button"><p class="centraize">メールでのお問い合わせ</p></div></a>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-</div>
+    </section>
+  </div>
+</section>
