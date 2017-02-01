212614e00cf8aac0bb351db87f08d7a05537b4ee
HiromuK <saga.cap319@gmail.com>
2017-02-01 23:08:25 +0900

    hedear.php(インクルード)アップ

---
+++ divorce/include/header.php
@@ -0,0 +1,36 @@
+<header class="lp4__header">
+  <div class="header__wrapper">
+    <div class="header__childwrapper-left">
+      <a href="/divorce/">
+        <img src="https://a-dreamlaw.com/common/images/logo.png" class="header__logo">
+      </a>
+      <p class="centraize header-mainhedding">離婚相談</p>
+    </div>
+    <div class="header__childwrapper-right">
+      <div class="header__childwrapper-right_left">
+        <a href="https://a-dreamlaw.com/contact/">
+          <div class="button button-header">
+            <p class="button-header_paragraph">
+              <i class="fa fa-envelope-o" aria-hidden="true"></i> 法律相談予約
+            </p>
+          </div>
+        </a>
+        <a href="tel:0335258010" class="lp4__sumaho-display" style="float: right; width: 56%;">
+          <div class="button" style="padding: 14px;">
+            <p class="button-header_paragraph">
+              <i class="fa fa-phone" aria-hidden="true"></i> お電話でのお問い合わせ
+            </p>
+          </div>
+        </a>
+      </div>
+      <div class="header__childwrapper-right_left header__childwrapper-right_left-sp-padding">
+        <a href="tel:0335258010" class="header__tel_number"><i class="fa fa-phone" aria-hidden="true"></i> 03-3525-8010</a>
+        <p class="header__message">初回の法律相談無料</p>
+        <p class="header__time">
+          営業時間：9:00〜21:00（平日）　9:00〜18:00（土日祝）<br>
+          受付時間：24時間対応
+        </p>
+      </div>
+    </div>
+  </div>
+</header>
