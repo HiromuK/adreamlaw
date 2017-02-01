9a333d8c816aaa654336e7b622c91bed51d2fdb1
HiromuK <saga.cap319@gmail.com>
2017-02-01 22:09:41 +0900

    インクルードファイルのリンク正常化

--- divorce/include/footer.php
+++ divorce/include/footer.php
@@ -1,50 +1,48 @@
 <footer class="footer">
-    <div class="footer__wrapper">
-      <div class="footer__box">
-        <ul class="footer__arrow">
-          <li><a href="/divorce/contents/separate.html">早めに相談するメリット</a></li>
-          <li><a href="/divorce/contents/separate.html">弁護士紹介</a></li>
-          <li><a href="/divorce/contents/separate.html">弁護士費用</a></li>
-          <li><a href="/divorce/contents/separate.html">当法人について</a></li>
-          <li><a href="/divorce/contents/separate.html">サイトマップ</a></li>
-          <li><a href="/divorce/contents/separate.html">プライバシーポリシー</a></li>
-        </ul>
-      </div>
-      <div class="footer__box">
-        <ul class="footer__arrow">
-          <li><a href="/divorce/contents/separate.html">慰謝料のホント</a></li>
-          <li><a href="/divorce/contents/separate.html">離婚とお金</a></li>
-          <li><a href="/divorce/contents/separate.html">離婚と子ども</a></li>
-          <li><a href="/divorce/contents/separate.html">「別居」が意味するもの</a></li>
-          <li><a href="/divorce/contents/separate.html">離婚Q＆A</a></li>
-          <li><a href="/divorce/contents/separate.html">解決事例</a></li>
-        </ul>
-      </div>
-      <div class="footer__links-sns-box">
-          <p><a href="http://ameblo.jp/awajichodream/" rel="nofollow" target="blank" class="fade"><img src="/common/images/blog.jpg" alt="淡路町ドリーム公式ブログ"></a></p>
-          <p><a href="http://myben54.com/" rel="nofollow" target="blank" class="fade"><img src="/common/images/myben54bannar.jpg" alt="MY弁護士"></a></p>
-        <ul>
-          <li class="sns_ico"><a href="https://ja-jp.facebook.com/pages/%E5%BC%81%E8%AD%B7%E5%A3%AB%E6%B3%95%E4%BA%BA%E6%B7%A1%E8%B7%AF%E7%94%BA%E3%83%89%E3%83%AA%E3%83%BC%E3%83%A0/1440368016199684" rel="nofollow" class="fade" target="blank"><img src="/common/images/icon/facebook.jpg" width="50" height="50" alt="淡路町ドリームfacebook公式アカウント"></a></li>
-          <li class="sns_ico"><a href="http://twitter.com/adreamlaw" rel="nofollow" class="fade" target="blank"><img src="/common/images/icon/twitter.jpg" width="50" height="50" alt="淡路町ドリームTwitter公式アカウント"></a></li>
-        </ul>
-      </div>
-      <div class="footer__access">
-        <div class="footer__access-googlemap">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1620.1179726426956!2d139.76765000000003!3d35.695811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c035538e8a5%3A0x38cf7382d6b69aa1!2z5reh6Lev55S644OJ44Oq44O844Og77yI5byB6K235aOr5rOV5Lq677yJ!5e0!3m2!1sja!2sjp!4v1434260231385"	 frameborder="0" style="border:0"></iframe>
-        </div>
-        <aside>
-          <p class="footer__access-aside"><i class="fa fa-map-marker" aria-hidden="true"></i> 東京都千代田区神田淡路町1-2　クリスタルビル 3F 4F・受付8F<br>
-            ※スターバックスが入っているビルの8Fが受付になります。
-          </p>
-        </aside>
-        <ul class="footer__access-lists">
-          <li class="footer__access-list"><i class="fa fa-subway" aria-hidden="true"></i> 東京メトロ丸の内線 淡路町駅 A3・A5出口より徒歩1分</li>
-          <li class="footer__access-list"><i class="fa fa-subway" aria-hidden="true"></i> 都営地下鉄新宿線 小川町駅 A3出口より徒歩1分</li>
-          <li class="footer__access-list"><i class="fa fa-subway" aria-hidden="true"></i> 東京メトロ千代田線 新御茶ノ水駅 B4出口より徒歩3分</li>
-          <li class="footer__access-list"><i class="fa fa-train" aria-hidden="true"></i> JR 御茶ノ水駅 「聖橋口」出口より徒歩12分</li>
-          <li class="footer__access-list"><i class="fa fa-train" aria-hidden="true"></i> JR 神田駅 北口改札より徒歩10分</li>
-        </ul>
-      </div>
+  <div class="footer__wrapper">
+    <div class="footer__box">
+      <ul class="footer__arrow">
+        <li><a href="/divorce/contents/merit.html">早めに相談するメリット</a></li>
+        <li><a href="/divorce/lawyer/">弁護士紹介</a></li>
+        <li><a href="/divorce/fee/">弁護士費用</a></li>
+        <li><a href="/office/">当法人について</a></li>
+        <li><a href="/sitemap/">サイトマップ</a></li>
+        <li><a href="/public/">プライバシーポリシー</a></li>
+      </ul>
     </div>
-      <p class="centraize footer__copyright">Copyright (c) 2016 Copyright 弁護士法人淡路町ドリーム All Rights Reserved.</p>
+    <div class="footer__box">
+      <ul class="footer__arrow">
+        <li><a href="/divorce/contents/actual-compensation.html">慰謝料のホント</a></li>
+        <li><a href="/divorce/contents/money/">離婚とお金</a></li>
+        <li><a href="/divorce/contents/child/">離婚と子供</a></li>
+        <li><a href="/divorce/contents/separate.html">「別居」が意味するもの</a></li>
+        <li><a href="/divorce/contents/qa/">離婚問題のQ&amp;A</a></li>
+        <li><a href="/divorce/case/">解決事例</a></li>
+      </ul>
+    </div>
+    <div class="footer__links-sns-box">
+      <p><a href="http://ameblo.jp/awajichodream/" rel="nofollow" target="blank" class="fade"><img src="/common/images/blog.jpg" alt="淡路町ドリーム公式ブログ"></a></p>
+      <p><a href="http://myben54.com/" rel="nofollow" target="blank" class="fade"><img src="/common/images/myben54bannar.jpg" alt="MY弁護士"></a></p>
+      <ul>
+        <li class="sns_ico"><a href="https://ja-jp.facebook.com/pages/%E5%BC%81%E8%AD%B7%E5%A3%AB%E6%B3%95%E4%BA%BA%E6%B7%A1%E8%B7%AF%E7%94%BA%E3%83%89%E3%83%AA%E3%83%BC%E3%83%A0/1440368016199684" rel="nofollow" class="fade" target="blank"><img src="/common/images/icon/facebook.jpg" width="50" height="50" alt="淡路町ドリームfacebook公式アカウント"></a></li>
+        <li class="sns_ico"><a href="http://twitter.com/adreamlaw" rel="nofollow" class="fade" target="blank"><img src="/common/images/icon/twitter.jpg" width="50" height="50" alt="淡路町ドリームTwitter公式アカウント"></a></li>
+      </ul>
+    </div>
+    <div class="footer__access">
+      <div class="footer__access-googlemap">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1620.1179726426956!2d139.76765000000003!3d35.695811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c035538e8a5%3A0x38cf7382d6b69aa1!2z5reh6Lev55S644OJ44Oq44O844Og77yI5byB6K235aOr5rOV5Lq677yJ!5e0!3m2!1sja!2sjp!4v1434260231385"   frameborder="0" style="border:0"></iframe>
+      </div>
+      <aside>
+        <p class="footer__access-aside"><i class="fa fa-map-marker" aria-hidden="true"></i> スターバックスが入っているビルの8Fが受付になります。</p>
+      </aside>
+      <ul class="footer__access-lists">
+        <li class="footer__access-list"><i class="fa fa-subway" aria-hidden="true"></i> 東京メトロ丸の内線 淡路町駅 A3・A5出口より徒歩1分</li>
+        <li class="footer__access-list"><i class="fa fa-subway" aria-hidden="true"></i> 都営地下鉄新宿線 小川町駅 A3出口より徒歩1分</li>
+        <li class="footer__access-list"><i class="fa fa-subway" aria-hidden="true"></i> 東京メトロ千代田線 新御茶ノ水駅 B4出口より徒歩3分</li>
+        <li class="footer__access-list"><i class="fa fa-train" aria-hidden="true"></i> JR 御茶ノ水駅 「聖橋口」出口より徒歩12分</li>
+        <li class="footer__access-list"><i class="fa fa-train" aria-hidden="true"></i> JR 神田駅 北口改札より徒歩10分</li>
+      </ul>
+    </div>
+  </div>
+  <p class="centraize footer__copyright">Copyright (c) 2016 Copyright 弁護士法人淡路町ドリーム All Rights Reserved.</p>
 </footer>
--- divorce/include/globalmenu.php
+++ divorce/include/globalmenu.php
@@ -1,7 +1,7 @@
 <nav class="globalmenu">
   <ul class="globalmenu__lists">
     <a href="/divorce/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 離婚TOP</li></a>
-    <a href="contents/merit.html"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士に早めに相談するメリット</li></a>
+    <a href="/divorce/contents/merit.html"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士に早めに相談するメリット</li></a>
     <a href="/divorce/lawyer/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士紹介</li></a>
     <a href="/divorce/case/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 解決実績</li></a>
     <a href="/divorce/fee/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士費用</li></a>
