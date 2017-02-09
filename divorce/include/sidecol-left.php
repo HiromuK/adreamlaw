ab6fb2085aac12f1b9007b306e8ed2910e53fdb9
Masterwood <kimura.unit@gmail.com>
2017-02-10 06:44:55 +0900

    左カラムのリンク先修正

--- divorce/include/sidecol-left.php
+++ divorce/include/sidecol-left.php
@@ -39,7 +39,7 @@
     </section>
 
     <section>
-      <a href="/divorce/contents/money/">
+      <a href="/divorce/contents/divorce-money.html">
       <div class="sidecontents__border sidecolumn__button-fade">
         <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">考えるべきこと</h4>
         <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">離婚とお金</p>
@@ -48,7 +48,7 @@
     </section>
 
     <section>
-      <a href="/divorce/contents/child/">
+      <a href="/divorce/contents/divorce-child.html">
       <div class="sidecontents__border sidecolumn__button-fade">
         <h4 class="centraize sidecontents__subhedding sidecontents__paragraph">考えるべきこと</h4>
         <p class="centraize sidecontents__emphasis sidecontents__paragraph-only">離婚と子ども</p>
