67d3e86c1f37aef16622ce9ef4bb71bfe1457670
HiromuK <saga.cap319@gmail.com>
2017-03-04 14:44:49 +0900

    sp用グローバルメニュー追加

--- divorce/include/globalmenu.php
+++ divorce/include/globalmenu.php
@@ -8,3 +8,19 @@
     <a href="/contact/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 問い合わせ</li></a>
   </ul>
 </nav>
+
+<!-- sp用グローバルメニュー -->
+<button type="button" class="drawer-toggle drawer-hamburger">
+  <span class="sr-only">toggle navigation</span>
+  <span class="drawer-hamburger-icon"></span>
+</button>
+<nav class="drawer-nav" role="navigation">
+  <ul class="drawer-menu">
+    <li><a class="drawer-brand" href="/divorce/">離婚TOP</a></li>
+    <li><a class="drawer-menu-item" href="/divorce/contents/merit.html"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士に早めに相談するメリット</a></li>
+    <li><a class="drawer-menu-item" href="/divorce/lawyer/"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士紹介</a></li>
+    <li><a class="drawer-menu-item" href="/divorce/case/"><i class="fa fa-chevron-right" aria-hidden="true"></i> 解決実績</a></li>
+    <li><a class="drawer-menu-item" href="/divorce/fee/"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士費用</a></li>
+    <li><a class="drawer-menu-item" href="/contact/"><i class="fa fa-chevron-right" aria-hidden="true"></i> 問い合わせ</a></li>
+  </ul>
+</nav>
