6dd20ef01606fd8612284489ace80ed10fc5196f
Masterwood <kimura.unit@gmail.com>
2017-03-13 15:29:15 +0900

    divorceの左カラムメニューのスマホ時のアコーディオンを調整

--- divorce/js/accordion.js
+++ divorce/js/accordion.js
@@ -1,13 +1,6 @@
 $(function(){
-    $(".acMenu1 p").click(function(){
+    $(".sidecolumn-sp__acordion_lists p").click(function(){
       $(this).next("ul").slideToggle();
       $(this).children("span").toggleClass("active");
-    });
-
-    $(".acMenu2 dt").on("click", function() {
-      $(this).next("dd").slideToggle();
-      $(this).next("dd").siblings("dd").slideUp();
-      $(this).toggleClass("active");
-      $(this).siblings("dt").removeClass("active");
     });
 });
