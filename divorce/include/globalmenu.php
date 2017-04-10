574f60baaa6a866fc77a6cc4ae8b29a9ae6e63c8
Masterwood <kimura.unit@gmail.com>
2017-04-10 16:49:54 +0900

    divorce内にlp2を移植した。

--- divorce/include/globalmenu.php
+++ divorce/include/globalmenu.php
@@ -3,6 +3,7 @@
     <a href="/divorce/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 離婚TOP</li></a>
     <a href="/divorce/contents/merit.html"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士に早めに相談するメリット</li></a>
     <a href="/divorce/lawyer/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士紹介</li></a>
+    <a href="/divorce/intro/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> あたなは離婚で幸せになれる？</li></a>
     <a href="/divorce/case/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 解決実績</li></a>
     <a href="/divorce/fee/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士費用</li></a>
     <a href="/contact/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> 問い合わせ</li></a>
@@ -19,6 +20,7 @@
     <li><a class="drawer-brand" href="/divorce/">離婚TOP</a></li>
     <li><a class="drawer-menu-item" href="/divorce/contents/merit.html"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士に早めに相談するメリット</a></li>
     <li><a class="drawer-menu-item" href="/divorce/lawyer/"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士紹介</a></li>
+    <a href="/divorce/intro/"><li class="globalmenu__list"><i class="fa fa-chevron-right" aria-hidden="true"></i> あたなは離婚で幸せになれる？</li></a>
     <li><a class="drawer-menu-item" href="/divorce/case/"><i class="fa fa-chevron-right" aria-hidden="true"></i> 解決実績</a></li>
     <li><a class="drawer-menu-item" href="/divorce/fee/"><i class="fa fa-chevron-right" aria-hidden="true"></i> 弁護士費用</a></li>
     <li><a class="drawer-menu-item" href="/contact/"><i class="fa fa-chevron-right" aria-hidden="true"></i> 問い合わせ</a></li>
