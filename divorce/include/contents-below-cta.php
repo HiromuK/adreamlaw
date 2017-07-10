4a3c48ccf3afd56dca16c1fd37ddf8b205526361
Masterwood <kimura.unit@gmail.com>
2017-07-10 17:28:10 +0900

    離婚特設ページのデザインをコンテンツ右寄せから左寄せに変えた。トップの画像も変えた。

--- divorce/include/contents-below-cta.php
+++ divorce/include/contents-below-cta.php
@@ -9,7 +9,7 @@
         <li class="trouble-list">別居時の新住所を相手にバレたくない！</li>
         <li class="trouble-list">離婚するのに有利な別居期間はどれくらい？</li>
       </ul>
-      <div class="trouble-img"><img src="/divorce/images/woman-trouble.jpg" alt="悩みを抱える女性" style="width:90%; margin:20px"></div>
+      <div class="trouble-img"><img src="/divorce/images/price-table-show.jpg" alt="悩みを抱える女性" style="width:90%; margin:20px"></div>
       <p class="mention"><span class="font-red">今すぐ離婚が得意な弁護士に、別居時に取るべき行動をご相談ください！</span></p>
       <div class="cta__sub-wrapper">
         <a onclick="ga('send', 'event', 'CTA', 'Click', 'tel');" href="tel:0335258010"><div class="cta__button"><p class="centraize">03-3525-8010</p></div></a>
