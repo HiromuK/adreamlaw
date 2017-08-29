91db93f4b7dfd37a714577b09c94db50b8cc3183
HiromuK <saga.cap319@gmail.com>
2017-08-29 12:55:42 +0900

    タグ設置場所の最適化と共通スクリプトを集約したテストファイルの追加

---
+++ include/header-ha.php
@@ -0,0 +1,47 @@
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WL2L4J"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<!-- Ptengine -->
+<script type="text/javascript">
+  window._pt_lt = new Date().getTime();
+  window._pt_sp_2 = [];
+   _pt_sp_2.push('setAccount,170c650d');
+  var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+  (function() {
+    var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
+     atag.src = _protocol + 'js.ptengine.jp/pta.js';
+    var stag = document.createElement('script'); stag.type = 'text/javascript'; stag.async = true;
+     stag.src = _protocol + 'js.ptengine.jp/pts.js';
+    var s = document.getElementsByTagName('script')[0];
+     s.parentNode.insertBefore(atag, s); s.parentNode.insertBefore(stag, s);
+    })();
+</script>
+<!-- End Ptengine -->
+
+<header style="padding:0;">
+	<div class="row bk_white">
+		<div class="header_inner row container">
+			<div class="header_left col span_6">
+				<p id="page-top"><a href="#wrap" class="fade"><img src="/common/images/page_top.jpg" class="img_rounded"><br><span class="font_s">TOPへ</span></a></p>
+				<div class="logo spnone">
+					<a href="/"><img src="/common/images/logo.png" width="308" height="47" alt="弁護士法人淡路町ドリーム"></a>
+	        <p class="sp_txt_C spnone">営業　8:00～21:00（平日）/ 9:00～18:00（土日・祝）　受付 24時間対応</p>
+				</div>
+			</div>
+			<div class="header_right col span_6">
+				<div class="info">
+					<figure>
+						<a href="/contact/" id="form_pchead_gtm" class="fade"><img src="/common/images/form_contact.jpg"　width="408" height="67" alt="無料相談、土日祝日も相談可能！お気軽にご相談ください。03-3525-8010"></a>
+					</figure>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="pcnone">
+		<ul class="header_contact_sp2 row">
+			<li class="header_sp_left"><a href="tel:03-3525-8010" onclick="ga('send', 'event', 'CTA', 'Click', 'tel');"><img src="/common/images/header_tel.jpg" alt="Call:03-3525-8010" width="400" height="100" onclick="yahoo_report_conversion(undefined); goog_report_conversion('tel:03-3525-8010')" /></a></li>
+			<li class="header_sp_right"><a href="/contact/"><img src="/common/images/header_mail_sp.jpg" alt="メールでのお問い合わせ"></a></li>
+		</ul>
+	</div>
